e9a5e9545f3a580c73a21c1fcbbbf33d5065946f
Andreas Berrou <andreas.berrou@hec.de>
2019-12-18 07:43:53 +0100

    add test

--- testing/cypress/cypress/integration/teamonetest.spec.js
+++ testing/cypress/cypress/integration/teamonetest.spec.js
@@ -1,4 +1,7 @@
 /// <reference types="Cypress" />
-it('should do something', () => {
-    cy.visit(www.devopswiki.de)
+it('should do this', () => {
+    cy.visit('www.devopswiki.de')
+})
+it('should do that', () => {
+    cy.visit('www.google.de')
 })
