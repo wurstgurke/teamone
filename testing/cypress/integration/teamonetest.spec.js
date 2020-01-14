6b6a67d03417b2733e97d2f5e220f7669560b225
Andreas Berrou <andreas.berrou@hec.de>
2020-01-14 13:21:54 +0100

    npm change

--- testing/cypress/integration/teamonetest.spec.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="Cypress" />
-it('should open the backend', () => {
-    cy.visit('http://h2862062.stratoserver.net:80/backend')
-})
-it('should open the frontend', () => {
-    cy.visit('http://h2862062.stratoserver.net:80/frontend')
-})
