be317a917d7467af2dc385ff79801b8c08e6ae8d
Andreas B <andreas@berrou.de>
2020-01-26 20:13:41 +0100

    Add db and new backend

---
+++ backend/src/main/java/com/teamone/teamone/TeamoneApplication.java
@@ -0,0 +1,13 @@
+package com.teamone.teamone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamoneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/teamone/teamone/TeamoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamone.teamone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
